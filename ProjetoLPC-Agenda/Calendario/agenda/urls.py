6a5d8b223adbab08c752657a1f9ab40f803f5907
Joao Vitor Moreno <jvmoreno99@gmail.com>
2018-04-27 15:43:07 -0300

    Update urls.py
    Alterado para sintaxe Django 2.x

--- ProjetoLPC-Agenda/Calendario/agenda/urls.py
+++ ProjetoLPC-Agenda/Calendario/agenda/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from .views import *
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^all/', todasAgendas, name='all'),
-    url(r'^agenda/usuario/([0-9]{1})/', get_userAgendas_byID),
-    url(r'^compromisso/', agendaInstitucional, name="Institucional")
+    path('admin/', admin.site.urls),
+    path('all/', todasAgendas, name='all'),
+    path('agenda/usuario/<int:([0-9]{1})>/', get_userAgendas_byID),
+    path('compromisso/', agendaInstitucional, name="Institucional")
 ]
